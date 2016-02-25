e747f5e9c2893f9cac7e441f8615fbd658289b76
ashtastic <amurr20@gmail.com>
2016-02-25 13:09:57 -0500

    Set up the variables.

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var Intro = alert('Let’s make a story together!')
+
+var Color = prompt('A colour (orange, indigo, etc.)');
+
+var Creature = prompt('A creature (dragon, moth, etc.)');
+
+var Adjective = prompt('An adjective (beautiful, super, etc.)');
+
+var Verb = prompt('A past tense verb (ran, burped, etc.)');
+
+var Story = prompt('Which story would you like?\n[a] Snacking\n[b] Escaping\n[c] Winning\nEnter the letter below.');
+
+var aStory = 'a'
+
+var bStory = 'b'
+
+var cStory = 'c'
+
+if (aStory && color !=null && creature !=null && adjective !=null && verb !=null) {
+    document.write('After screaming for literally an hour,' + color + 'haired' + creature + verb + 'for hours.');
+  } else if (bStory && color !=null && creature !=null && adjective !=null && verb !=null) {
+    document.write('Before stuffing the entire,' + color + 'coloured' + creature + 'in their mouth, they' + verb + 'until the end of their days.')
+  } else (cStory && color !=null && creature !=null && adjective !=null && verb !=null) {
+    document.write('After the joy ride, the' + color + creature + verb + 'all the way home.')
+  }
+
+
+
+After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.
+
+Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
+
+Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
