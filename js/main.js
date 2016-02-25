060e851bfe577edbf0f4bd41c91379321db12871
ashtastic <amurr20@gmail.com>
2016-02-25 13:39:11 -0500

    Markbot is satisfied.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var intro = alert('Let’s make a story together!')
+var intro = alert('Let’s make a story together!');
 
 var colour = prompt('A colour (orange, indigo, etc.)');
 
@@ -10,19 +10,20 @@ var verbed = prompt('A past tense verb (ran, burped, etc.)');
 
 var story = prompt('Which story would you like?\n[a] Snacking\n[b] Escaping\n[c] Winning\nEnter the letter below.');
 
-var story = [a, b, c]
+var abcStory = ['a', 'b', 'c'];
 
-switch (story) {
+switch (abcStory) {
 
   case 'a' :
-    document.write('After snacking on' + ' ' + adjective + ' ' + 'treats, the' + ' ' + colour + ' ' + 'bellied ' + ' ' + creature + ' ' + verbed + ' ' + 'for hours.')
+    document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
     break;
 
   case 'b' :
-    document.write('Ameilia' + ' ' + verbed + ' ' + 'through the ' + ' ' + adjective + ' ' + colour + ' ' + 'nebula escaping the space' + ' ' + creature + '.')
+    document.write('Ameilia ' + verbed + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature + ' .');
     break;
 
   case 'c' :
-    document.write('Jackson chose his' + ' ' + adjective + ',' + ' ' + colour + ' ' + creature + 'card and' + ' ' + verbed + ' ' + 'it to the table knowing he won.')
+    document.write('Jackson chose his ' + adjective + ', ' + colour + ' ' + creature + ' card and ' + verbed + ' it to the table knowing he won.');
     break;
 }
+
