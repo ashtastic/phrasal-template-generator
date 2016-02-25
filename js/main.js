87c93d81e66f2c324b5d26efd042b8108eadb1ef
ashtastic <amurr20@gmail.com>
2016-02-25 13:27:21 -0500

    Added proper content.

--- js/main.js
+++ js/main.js
@@ -1,33 +1,28 @@
-var Intro = alert('Let’s make a story together!')
+var intro = alert('Let’s make a story together!')
 
-var Color = prompt('A colour (orange, indigo, etc.)');
+var colour = prompt('A colour (orange, indigo, etc.)');
 
-var Creature = prompt('A creature (dragon, moth, etc.)');
+var creature = prompt('A creature (dragon, moth, etc.)');
 
-var Adjective = prompt('An adjective (beautiful, super, etc.)');
+var adjective = prompt('An adjective (beautiful, super, etc.)');
 
-var Verb = prompt('A past tense verb (ran, burped, etc.)');
+var verbed = prompt('A past tense verb (ran, burped, etc.)');
 
-var Story = prompt('Which story would you like?\n[a] Snacking\n[b] Escaping\n[c] Winning\nEnter the letter below.');
+var story = prompt('Which story would you like?\n[a] Snacking\n[b] Escaping\n[c] Winning\nEnter the letter below.');
 
-var aStory = 'a'
+var story = [a, b, c]
 
-var bStory = 'b'
+switch (story) {
 
-var cStory = 'c'
+  case 'a' :
+    document.write('After snacking on' + ' ' + adjective + ' ' + 'treats, the' + ' ' + colour + ' ' + 'bellied ' + ' ' + creature + ' ' + verbed + ' ' + 'for hours.')
+    break;
 
-if (aStory && color !=null && creature !=null && adjective !=null && verb !=null) {
-    document.write('After screaming for literally an hour,' + color + 'haired' + creature + verb + 'for hours.');
-  } else if (bStory && color !=null && creature !=null && adjective !=null && verb !=null) {
-    document.write('Before stuffing the entire,' + color + 'coloured' + creature + 'in their mouth, they' + verb + 'until the end of their days.')
-  } else (cStory && color !=null && creature !=null && adjective !=null && verb !=null) {
-    document.write('After the joy ride, the' + color + creature + verb + 'all the way home.')
-  }
+  case 'b' :
+    document.write('Ameilia' + ' ' + verbed + ' ' + 'through the ' + ' ' + adjective + ' ' + colour + ' ' + 'nebula escaping the space' + ' ' + creature + '.')
+    break;
 
-
-
-After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.
-
-Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
-
-Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
+  case 'c' :
+    document.write('Jackson chose his' + ' ' + adjective + ',' + ' ' + colour + ' ' + creature + 'card and' + ' ' + verbed + ' ' + 'it to the table knowing he won.')
+    break;
+}
